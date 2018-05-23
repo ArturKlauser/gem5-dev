0c31f7b7be249443c54eb968cd6b09c89b38059f
Artur Klauser <Artur.Klauser@computer.org>
2018-05-23 21:04:16 +0200

    Checking in initial Docker files and description.

--- README.md
+++ README.md
@@ -1,2 +1,106 @@
-# gem5-dev
-gem5 ARM development environment docker image
+# Build environment for gem5 ARM.
+The Dockerfile in this repository creates the gem5-dev docker image that
+includes the build environment necessary to compile and run the gem5 ARM
+full system simulator. The image is structured in such a way that it assumes
+the user mounts a gem5 working directory residing on the host machine into
+the container running the gem5-dev image. The docker image hosts the full
+build environment, whereas the gem5 working directory holds the gem5 source
+code, compile artifacts, and the image file for full system simulation if
+desired.
+
+The gem5-dev docker image is structured to run as a binary supporting several
+commands:
+  * install-source ... installs the gem5 sources from git in the working
+     directory
+  * update-source ... updates the gem5 sources
+  * install-system ... installs the gem5 system image by downloading and
+    uplacking the corresponding tar file
+  * build ... builds the gem5 ARM binary
+  * run-se ... runs a sample binary in syscall emulation mode
+  * run-fs ... starts a sample run in full system mode
+  * shell ... enters into an interactive shell in the container running the
+    gem5-dev image in which the developer can work within the build
+    environment; above commands are also available within this shell as
+    commands to the gem5-dev tool
+
+## How to build the docker image
+Build the gem5-dev image like this:
+```
+docker build -t gem5-dev docker
+```
+This creates the docker image called gem5-dev.
+
+## How to use the docker image
+### Setting up the gem5 working directory
+Create a directory on the host system which you'd like to use as gem5
+working directory. It will contain the sources, build artifacts (e.g. .o and
+binary files), and the ARM system image files. You'll need several GBs of
+space for this (currently about 12 GB, but it depends on your builds). 
+Let's call that directory $GEM5_WORKDIR.
+
+### Installing, building, and running gem5
+Now you can run the gem5-dev docker image and use it to get the gem5 source,
+compile gem5, and run it:
+``
+docker run --rm -v $GEM5_WORKDIR:/gem5 -it gem5-dev install-source
+docker run --rm -v $GEM5_WORKDIR:/gem5 -it gem5-dev build
+docker run --rm -v $GEM5_WORKDIR:/gem5 -it gem5-dev run-se
+```
+Note that the gem5 sources will be installed into a directory called
+'source' inside $GEM5_WORKDIR.
+
+### Installing and running full system images
+If you also want to use full system mode, you have to install the
+corresponding system files as well:
+```
+docker run --rm -v $GEM5_WORKDIR:/gem5 -it gem5-dev install-system
+docker run --rm -v $GEM5_WORKDIR:/gem5 -it gem5-dev run-fs
+```
+Note that the gem5 ARM system file will be installed into a directory called
+'system' inside $GEM5_WORKDIR.
+
+### Starting your own runs
+The runs that are started are just sample commands that allow you to verify
+that the build has worked properly. In order to actually do something useful
+with gem5, you'll have to issue your own run commands. In order to do that,
+get into a shell in the build environment and issue the run commands you
+need:
+```
+docker run --rm -v $GEM5_WORKDIR:/gem5 -it gem5-dev shell
+cd source
+build/ARM/gem5.opt configs/example/se.py -c tests/test-progs/hello/bin/arm/linux/hello
+```
+
+### Modifying source code
+The gem5-dev build environment doesn't contain any editor, so it's not a
+convenient place to modify the gem5 sources. The method of development with
+the gem5-dev docker image is that you edit the gem5 source files on your
+host machine on which it is assumed you have already set up whatever source
+code editing environment you prefer. Since all the source code lives in the
+gem5 working dir on the host your host editing environment has full access
+to the source files. Once you're done with a modification and want to test
+it, you simply run the compile step inside the gem5-dev container. Here is a
+sample development cycle:
+```
+vim $GEM5_WORKDIR/source/src/arch/arm/some_file.cc
+# edit that file, then compile it
+docker run --rm -v $GEM5_WORKDIR:/gem5 -it gem5-dev build
+```
+
+Alternatively, you could also use a second terminal in which you keep a
+shell open in the gem5-dev container and perform your compiles there:
+
+```
+# Terminal 1:
+docker run --rm -v $GEM5_WORKDIR:/gem5 -it gem5-dev shell
+cd source
+
+# Terminal 2:
+vim $GEM5_WORKDIR/source/src/arch/arm/some_file.cc
+# edit that file
+
+# Terminal 1:
+scons -j $(nproc) build/ARM/gem5.opt
+# if it works, run it, otherwise back to editing
+build/ARM/gem5.opt configs/example/se.py -c tests/test-progs/hello/bin/arm/linux/hello
+```
---
+++ docker/gem5-dev.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+#
+# This script is the front-end execution script for using the gem5-dev
+# docker image. To use it, you would run a command like this:
+#   docker run -v $GEM5_HOST_WORKDIR:/gem5 -it gem5-dev [<cmd>]
+#
+# Author: Artur Klauser
+
+#MOUNTDIR#  # substituted during docker build
+mountdir=${mountdir:-'/gem5'}
+readonly sourcedir="${mountdir}/source"
+readonly systemdir="${mountdir}/system"
+
+print_usage() {
+  cat << EOF
+Usage: gem5-dev <cmd>
+Where <cmd> is one of:
+  help ............. prints this help message
+  install-source ... installs the gem5 git source repository into ${sourcedir}
+  update-source .... updates the gem5 git source repository in ${sourcedir}
+  install-system ... installs the ARM git5 system images in ${systemdir}
+  build ............ builds gem5 ARM binary
+  run-se ........... runs gem5 ARM in syscall emulation mode
+  run-fs ........... runs gem5 ARM in full system mode
+  shell | bash ..... enters into an interactive shell
+EOF
+}
+
+check_hostdir_mounted() {
+  # The docker image contains a watermark file in ${mountdir} which will
+  # only be visible when no volume is mounted.
+  if [[ -e "${mountdir}/.in-docker-container" ]]; then
+    cat << EOF
+No host volume mounted to container's ${mountdir} directory.
+Run:
+  docker run -v \$GEM5_HOST_WORKDIR:${mountdir} -it gem5-dev [<cmd>]
+EOF
+    exit 1
+  fi
+}
+
+# Clone gem5 source repository into ${sourcedir} if it isn't already there.
+install_source() {
+  check_hostdir_mounted
+  if [ ! -e "${sourcedir}/.git" ]; then
+    echo "installing gem5 source respository into ${sourcedir} ..."
+    git clone https://gem5.googlesource.com/public/gem5 ${sourcedir}
+  else
+    echo "gem5 source respository is already installed."
+  fi
+}
+
+# Pull updates from gem5 source repository.
+update_source() {
+  check_hostdir_mounted
+  if [[ -e "${sourcedir}/.git" ]]; then
+    echo "updatig gem5 source respository at ${sourcedir} ..."
+    cd ${sourcedir}
+    git pull
+  else
+    echo "gem5 source respository not found at ${sourcedir}."
+  fi
+}
+
+# Download full system image if it isn't aleady there.
+install_system() {
+  check_hostdir_mounted
+  if [[ ! -e "${systemdir}" ]]; then
+    echo "installing ARM full-system image into ${systemdir} ..."
+    mkdir "${systemdir}"
+    cd "${systemdir}"
+    readonly image='aarch-system-20180409.tar.xz'
+    wget "http://www.gem5.org/dist/current/arm/${image}"
+    tar xJvf "${image}"
+    # To save space we remove the tar ball.
+    rm "${image}"
+  else
+    echo "ARM full-system image is already installed."
+  fi
+}
+
+# Builds the gem5 ARM binary.
+build() {
+  check_hostdir_mounted
+  if [[ ! -e "${sourcedir}" ]]; then
+    echo "gem5 source respository not found at ${sourcedir}."
+    exit 1
+  fi
+
+  echo "building gem5 ARM binary ..."
+  cd ${sourcedir}
+  # Building inst-constrs-3.cc is a memory hog and can easily run the
+  # container out of resources if done in parallel with other compiles. So
+  # we first build it alone and then build the rest.
+  local cmd="scons build/ARM/arch/arm/generated/inst-constrs-3.o"
+  echo "${cmd}"
+  ${cmd}
+  # Now build the rest in parallel.
+  local cmd="scons -j $(nproc) build/ARM/gem5.opt"
+  echo "${cmd}"
+  ${cmd}
+}
+
+# Runs the gem5 ARM binary in syscall emulation mode.
+run_se() {
+  check_hostdir_mounted
+  if [[ ! -e "${sourcedir}" ]]; then
+    echo "gem5 source respository not found at ${sourcedir}."
+    exit 1
+  fi
+
+  echo "running gem5 ARM binary in syscall emulation mode ..."
+  cd ${sourcedir}
+  local simulator='build/ARM/gem5.opt'
+  local script='configs/example/se.py'
+  local binary='tests/test-progs/hello/bin/arm/linux/hello'
+  if [[ ! -e "${simulator}" ]]; then
+    echo "gem5 simulator binary ${simulator} not found."
+    exit 1
+  fi
+  cmd="${simulator} ${script} -c ${binary}"
+  echo "${cmd}"
+  ${cmd}
+}
+
+# Runs the gem5 ARM binary in full system mode.
+run_fs() {
+  check_hostdir_mounted
+  if [[ ! -e "${sourcedir}" ]]; then
+    echo "gem5 source respository not found at ${sourcedir}."
+    exit 1
+  fi
+  if [[ ! -e "${systemdir}" ]]; then
+    echo "gem5 ARM full system image not found at ${systemdir}."
+    exit 1
+  fi
+
+  echo "running gem5 ARM binary in full system mode ..."
+  cd ${sourcedir}
+  local simulator='build/ARM/gem5.opt'
+  local script='configs/example/fs.py'
+
+  if [[ ! -e "${simulator}" ]]; then
+    echo "gem5 simulator binary ${simulator} not found."
+    exit 1
+  fi
+  cmd="${simulator} ${script} \
+    --machine-type=VExpress_GEM5_V1 \
+    --dtb=armv8_gem5_v1_1cpu.dtb \
+    --kernel=vmlinux.vexpress_gem5_v1_64 \
+    --script=tests/halt.sh"
+  echo "${cmd}"
+  ${cmd}
+}
+
+# Starts an interactive shell.
+run_shell() {
+  check_hostdir_mounted
+  echo "To build gem5, run: "
+  echo "  cd ${sourcedir}; scons -j \$(nproc) build/ARM/gem5.opt"
+  cd "${mountdir}"
+  exec /bin/bash -l
+}
+
+main() {
+  local cmd="$1"
+  shift
+  case "${cmd}" in
+    'help') print_usage ;;
+    'install-source') install_source ;;
+    'update-source') update_source ;;
+    'install-system') install_system ;;
+    'build') build ;;
+    'run-se') run_se ;;
+    'run-fs') run_fs ;;
+    'shell' | 'bash') run_shell ;;
+    *)
+      echo "unkown command '$1'"
+      echo
+      print_usage
+      exit 1
+  esac
+}
+
+main "$@"
